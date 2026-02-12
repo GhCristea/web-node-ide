bf5a2043876d058235219d17cb91439bdea7823f
Gheorghita Cristea <gcristea73@gmail.com>
2026-02-12 01:59:28 +0000

    feat: App bootstrap with service initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { App } from './App'
+import { initializeServices } from './core/services'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// Initialize all core services before rendering
+initializeServices()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
-);
+  </React.StrictMode>,
+)
