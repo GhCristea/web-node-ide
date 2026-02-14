d100b65278519420f0dc9ea1527a56622b9326b3
Gheorghita Cristea <gcristea73@gmail.com>
2026-02-14 19:39:14 +0000

    feat: Integrate PWA functionality and configure cross-origin and caching headers

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 6 * 1024 * 1024, // 6MB to match sqlite3.wasm size and monaco workers
+        globPatterns: ['**/*.{js,css,html,wasm,svg,png,jpg}']
+      }
+    })
+  ],
   server: {
     headers: { 'Cross-Origin-Opener-Policy': 'same-origin', 'Cross-Origin-Embedder-Policy': 'require-corp' }
   },
